f443af80a613e7a14e69f60639bf0048464e62ed
Mengmei Tu <redmmeii@gmail.com>
2021-02-25 16:35:19 -0600

    update db

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -23,7 +23,7 @@ request.onerror = function (event) {
 
 function saveRecord(record) {
     // create a transaction on the pending db with readwrite access
-    const transaction = db.transaction(["pending"], "readwrite");
+    const transaction = db.transaction("pending", "readwrite");
 
     // access your pending object store
     const store = transaction.objectStore("pending");
@@ -34,7 +34,7 @@ function saveRecord(record) {
 
 function checkDatabase() {
     // open a transaction on your pending db
-    const transaction = db.transaction(["pending"], "readwrite");
+    const transaction = db.transaction("pending", "readwrite");
 
     // access your pending object store
     const store = transaction.objectStore("pending");
@@ -54,7 +54,7 @@ function checkDatabase() {
                 .then((response) => response.json())
                 .then(() => {
                     // if successful, open a transaction on your pending db
-                    const transaction = db.transaction(["pending", "readwrite"]);
+                    const transaction = db.transaction("pending", "readwrite");
 
                     // access your pending object store
                     const store = transaction.objectStore("pending");
